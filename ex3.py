875ffabed420e9d495da3cdf412cb96cde72e5a1
young-guwang <78337433+young-guwang@users.noreply.github.com>
2021-02-05 20:09:07 +0900

    Add files via upload

---
+++ ex3.py
@@ -0,0 +1,57 @@
+# 튜플
+# t1 = (1, "hello")
+# t2 = (3, 4.22)
+# t3 = t1 + t2
+# t4 = t1 * 3
+# e = t3[1]
+
+# print(t1)
+# print(t2)
+# print(t3)
+# print(t4)
+# print(e) 
+
+
+# 딕셔너리
+# dic = {1 : "My" , 2:"Name", 3:"Is", 4:"Python"}
+
+# print(dic[1])
+# print(dic[2])
+# print(dic[3])
+# print(dic[4])
+
+
+# 딕셔너리 내부 함수
+# dic = {1 : "My", 2 : "Name", 3 : "Is", 4 : "Python"}
+
+# print(dic.keys())
+# print(0 in dic.keys())
+# print(dic.values())
+# print(dic.items())
+
+
+
+# 리스트
+
+# myscores = [100,70,88,25]
+# yourscores = [55,46,100,98]
+
+# print(myscores)
+# print(yourscores)
+
+
+
+# 리스트 내부 함수
+# myscores = [100,70,88,25]
+# yourscores = [55,46,100,98]
+
+# myscores.sort()
+# yourscores.reverse()
+
+# print(myscores)
+# print(yourscores)
+
+# del myscores[0]
+# myscores.append(45)
+
+# print(myscores)
